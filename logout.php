dbdc0f1e197e5a4eed2e58702152243badb7ee25
netochaves <netobac1@gmail.com>
2017-01-18 01:25:25 -0200

    Adicionando o logout

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+  session_start();
+  if(isset($_SESSION['logado'])){
+     unset($_SESSION['logado']);
+     session_destroy();
+     header("Location: index.php");
+     exit();
+  }else{
+    header("Location: index.php");
+  }
+?>
